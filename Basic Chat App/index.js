6d9d77d344502c07e117ded9ff57c96f0d981c87
LeeJaeYun <ag502@naver.com>
2021-05-21 03:11:56 +0900

    socket disconnected

--- Basic Chat App/index.js
+++ Basic Chat App/index.js
@@ -15,6 +15,9 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected")
+  socket.on("disconnect", () => {
+    console.log("user disconnected")
+  })
 })
 
 
