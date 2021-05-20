c874962659aa36e332dc7f150755ebf9e84a97af
LeeJaeYun <ag502@naver.com>
2021-05-21 03:10:39 +0900

    apply socket.io

--- Basic Chat App/index.js
+++ Basic Chat App/index.js
@@ -1,15 +1,22 @@
 const express = require("express")
 const http = require("http")
+const {Server} = require("socket.io")
+
 
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.static(__dirname))
 
 app.get("/", (req, res) => {
-  console.log(__dirname)
   res.sendFile(__dirname + '/index.html')
 })
+
+io.on("connection", (socket) => {
+  console.log("a user connected")
+})
+
 
 server.listen(3000, () => {
   console.log("listening on *:3000")
